6b4baa8fc5d69ac598b8d97eec050355694da5f4
Jintao Koong <jintaokoong99@gmail.com>
2022-07-09 03:32:54 +0800

    chore: add comments

--- src/hooks/use-song-listing.ts
+++ src/hooks/use-song-listing.ts
@@ -6,9 +6,12 @@ import { flatten, map, values } from "ramda";
 const nestedValues = (
   option: Record<string, unknown> | unknown[]
 ): unknown[] => {
+  // if first level is of array, try expand each element recursively
   if (Array.isArray(option)) return map(nestedValues, option as any);
+  // if first level is an object, try unfold object then recursively call
   if (!Array.isArray(option) && typeof option === "object")
     return nestedValues(values(option));
+  // if first level is primitive, just return
   return option;
 };
 const extractValues = (options: ListingOptions) =>
--- src/services/song-service.ts
+++ src/services/song-service.ts
@@ -9,11 +9,14 @@ const extractProps = (
   value: Record<string, unknown> | unknown,
   key?: string
 ): object[] => {
+  // end case: if value is of primitive and key is defined, then return
   if (typeof value !== "object" && key) return [{ [key]: value }];
+  // start case: if key is not provided, expand the object
   if (typeof value === "object")
     return Object.entries(value!).map(([key, value]) =>
       extractProps(value, key)
     );
+  // intermediate case: recursively call function
   return extractProps(value, key);
 };
 
